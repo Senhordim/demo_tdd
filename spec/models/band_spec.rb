7cf127d92e72e93676ee2d420e5576b9e32ee028
Diego Collares <diego.collares@gmail.com>
2016-08-26 12:32:43 -0300

    teste de validação de presença

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Band, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validaçõa de presença no Modelo
+  it {is_expected.to validate_presence_of(:name)}
+  it {is_expected.to validate_presence_of(:musical_genre)}
+  it {is_expected.to validate_presence_of(:site)}
+
+
+  
+
+
 end
